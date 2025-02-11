bc7d15631938640f0b60a989276c15aa8effa0fe
saicharanShetty <68334178+saicharanShetty@users.noreply.github.com>
2025-02-11 16:08:19 +0530

    Create byte_swap.c

---
+++ C_Programs/Embedded_C/byte_swap.c
@@ -0,0 +1,7 @@
+//write a c code to byte swap and reverse a bits of given number.
+// Byte swap within a number ( swap 4th byte with 1st and 2nd with 3rd) [ used to convert endianess of a number ]
+	
+	int a=0x01020304;
+  int b= ((a&0xFF000000)>>24)|((a&0x00FF0000)>>8) |((a&0x0000FF00)<<8)|((a&0x000000FF)<<24);
+  printf("%x",b);
+	
